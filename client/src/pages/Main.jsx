f78b6644fc904824d83837cbb831c6e94d5fba72
Edwin Ajong <edwinajong@gmail.com>
2022-10-20 11:38:40 -0500

    first heroku push

--- client/src/pages/Main.jsx
+++ client/src/pages/Main.jsx
@@ -4,13 +4,15 @@ import { useLocation, useNavigate } from "react-router-dom";
 import { useState } from "react";
 import style from "../styles/Main.module.css";
 import logo from "../assets/images/logo.png";
+import TranscriptModal from "../components/TranscriptModal";
 
 function Main() {
   const location = useLocation();
   const navigate = useNavigate();
 
-  const [data, setData] = useState([]);
+  const [data, setData] = useState({});
   const [lastPage, setLastPage] = useState(0);
+  const [showTranscript, setShowTranscript] = useState(true);
 
   const handleNext = () => {
     const num = data.num + 1;
@@ -27,10 +29,16 @@ function Main() {
     navigate(`/${num}`);
   };
 
+  const handleShowTranscript = () => {
+    setShowTranscript(!showTranscript);
+    console.log("here");
+  };
+
   useEffect(() => {
     const fetchSpecificComic = async () => {
       const res = await axios.get(`/api${location.pathname}`);
       setData(res.data);
+      console.log(res.data.transcript);
       console.log(res.data);
       if (lastPage === 0) {
         const lastPageNum = await axios.get("/api/");
@@ -39,31 +47,64 @@ function Main() {
     };
 
     fetchSpecificComic();
-  }, [location, lastPage]);
+  }, [location]);
 
   return (
     <div className={style.body}>
-      <img src={logo} alt="" />
-      <div>
-        <h2 className={style.title}>{data && data.title}</h2>
-        <p>Date created: {`${data.year}-${data.month}-${data.day}`}</p>
-      </div>
+      <div className={style.content}>
+        <div className={style.logoDiv}>
+          <img className={style.logo} src={logo} alt="" />
+        </div>
+        <div className={style.buttons}>
+          <button className={style.button} onClick={handlePrev}>
+            Prev
+          </button>
 
-      <div className={style.imgDiv}>
-        {data && <img className={style.img} src={data.img} alt="" />}
-      </div>
-      {data && <p className={style.transcript}>{data.transcript}</p>}
-      <div className={style.buttons}>
-        <button className={style.button} onClick={handlePrev}>
-          Prev
-        </button>
+          <button className={style.button} onClick={handleRandom}>
+            Random
+          </button>
+          <button className={style.button} onClick={handleNext}>
+            Next
+          </button>
+        </div>
+        <div className={style.info}>
+          <h2 className={style.title}>{data && data.title}</h2>
+          <p className={style.date}>
+            Date created: {`${data.year}-${data.month}-${data.day}`}
+          </p>
+        </div>
 
-        <button className={style.button} onClick={handleRandom}>
-          Random
+        <div className={style.imgDiv}>
+          {data && <img className={style.img} src={data.img} alt="" />}
+        </div>
+        <button
+          onClick={handleShowTranscript}
+          className={style.TranscriptButton}
+        >
+          {showTranscript ? "Hide Transcript" : "Show Transcript"}
         </button>
-        <button className={style.button} onClick={handleNext}>
-          Next
-        </button>
+        <div>
+
+          {showTranscript &&
+            (data.transcript ? (
+              data.transcript.split("\n").map((text) => <p>{text}</p>)
+            ) : (
+              <p>No transcript Available</p>
+            ))}
+        </div>
+
+        <div className={style.buttons}>
+          <button className={style.button} onClick={handlePrev}>
+            Prev
+          </button>
+
+          <button className={style.button} onClick={handleRandom}>
+            Random
+          </button>
+          <button className={style.button} onClick={handleNext}>
+            Next
+          </button>
+        </div>
       </div>
     </div>
   );
