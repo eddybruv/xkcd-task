82e4e118c4db4fe6ef4b03cc033ed3e359154b95
Edwin Ajong <edwinajong@gmail.com>
2022-10-20 11:54:21 -0500

    updated server

--- server/server.js
+++ server/server.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const HomeRoute = require("./routes/Home.route");
+require("dotenv").config();
+
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
 app.use(express.json());
 app.use("/api", HomeRoute);
 
-const PORT = 5000;
+app.use(express.static(path.join(__dirname, "client", "build")));
+
+app.get("*", (_req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(PORT, () => console.log(`Running on port ${PORT}`));
