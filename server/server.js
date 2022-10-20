a6b153d1fdcce3c6a290526991482f852c76967b
Edwin Ajong <edwinajong@gmail.com>
2022-10-20 12:03:01 -0500

    updated postbuild script

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const HomeRoute = require("./routes/Home.route");
 require("dotenv").config();
+const path = require("path");
 
 const PORT = process.env.PORT || 5000;
 
@@ -9,10 +10,10 @@ const app = express();
 app.use(express.json());
 app.use("/api", HomeRoute);
 
-app.use(express.static(path.join(__dirname, "client", "build")));
+app.use(express.static(path.join(__dirname, "../", "client", "build")));
 
 app.get("*", (_req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  res.sendFile(path.join(__dirname, "../", "client", "build", "index.html"));
 });
 
 app.listen(PORT, () => console.log(`Running on port ${PORT}`));
