97a528be7812b8c51069c28d8dcde4e077047fcd
Edwin Ajong <edwinajong@gmail.com>
2022-10-20 20:31:04 -0500

    added the page count functionality

--- server/controllers/Home.controller.js
+++ server/controllers/Home.controller.js
@@ -1,23 +1,59 @@
 const axios = require("axios");
+const CountSchemaModel = require("../models/PageCount.model");
 let lastPage = 0;
 
 const HomeController = async (_req, res) => {
-  const results = await axios.get("https://xkcd.com/info.0.json");
-  lastPage = results.data.num;
-  res.status(200).json(results.data);
+  const { data } = await axios.get("https://xkcd.com/info.0.json");
+  lastPage = data.num;
+  const strip = await CountSchemaModel.find({ stripNumber: data.num });
+
+  if (strip.length === 0) {
+    const newStrip = await CountSchemaModel.create({
+      stripNumber: data.num,
+      count: 1,
+    });
+
+    return res.status(200).json({ ...data, count: newStrip.count });
+  } else {
+    const newCount = strip[0].count + 1;
+    const updatedStrip = await CountSchemaModel.findOneAndUpdate(
+      { stripNumber: data.num },
+      { count: newCount },
+      { new: true }
+    );
+
+    return res.status(200).json({ ...data, count: updatedStrip.count });
+  }
 };
 
 const SpecificController = async (req, res) => {
   const id = req.params.id;
 
-
   if (id === "NaN" || Number(id) > lastPage || Number(id) <= 0) {
     return res.status(400).json({ message: "page not found" });
   }
 
-  const results = await axios.get(`https://xkcd.com/${id}/info.0.json`);
+  const { data } = await axios.get(`https://xkcd.com/${id}/info.0.json`);
 
-  res.status(200).json(results.data);
+  const strip = await CountSchemaModel.find({ stripNumber: data.num });
+
+  if (strip.length === 0) {
+    const newStrip = await CountSchemaModel.create({
+      stripNumber: data.num,
+      count: 1,
+    });
+
+    return res.status(200).json({ ...data, count: newStrip.count });
+  } else {
+    const newCount = strip[0].count + 1;
+    const updatedStrip = await CountSchemaModel.findOneAndUpdate(
+      { stripNumber: data.num },
+      { count: newCount },
+      { new: true }
+    );
+
+    return res.status(200).json({ ...data, count: updatedStrip.count });
+  }
 };
 
 module.exports = { HomeController, SpecificController };
